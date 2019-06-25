611a4c2fa577aa4ce4116408643e6bc64fca202e
Tracey Birch <tracey.birch@sofwerx.org>
2019-06-25 10:16:04 -0400

    LetsEncrypt updates

--- run.sh
+++ run.sh
@@ -92,21 +92,21 @@ storage = "/ssl/acme.json"
 entryPoint = "https"
 EOF
 
-# Let's Encrypt ACME tls-sni-01 no longer works.
-# Let's Encrypt ACME http-01 only works on port 80 now.
-if [ "$HTTP_PORT" = "80" ]; then
-  cat <<EOF >> /etc/traefik/traefik.toml
-[acme.httpChallenge]
-  entryPoint = "http"
-EOF
-fi
-
 # Let's Encrypt ACME dns-01 works, if the environment variables are present.
 if [ -n "${AWS_ACCESS_KEY_ID}" ] ; then
 cat <<EOF >> /etc/traefik/traefik.toml
 [acme.dnsChallenge]
   provider = "route53"
   delayBeforeCheck = 0
+EOF
+fi
+
+# Let's Encrypt ACME tls-sni-01 no longer works.
+# Let's Encrypt ACME http-01 only works on port 80 now.
+if [ "$HTTP_PORT" = "80" ]; then
+  cat <<EOF >> /etc/traefik/traefik.toml
+[acme.httpChallenge]
+  entryPoint = "http"
 EOF
 fi
 
